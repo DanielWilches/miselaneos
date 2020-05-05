5d1e60b47f35f4f7358c64357341a3c05769be14
DanielEleazarPrietoWilches <software2@aprendizajesignificativo.org>
2020-05-04 22:18:10 -0500

    comiit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { FontAwesomeModule, FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { NgstyleComponent } from './components/ngstyle/ngstyle.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NgstyleComponent
   ],
   imports: [
     BrowserModule,
